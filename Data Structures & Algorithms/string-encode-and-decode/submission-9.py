f54f7c598453f92818febf22b02e5b0d285f6af8
Kashif Ayaz Khan <kashifayaz5252@gmail.com>
2026-06-29 01:30:26 +0100

    Add: string-encode-and-decode - submission-9

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-9.py
@@ -0,0 +1,50 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        output = ""
+        num_str = len(strs)
+        letters = []
+        output_list = []
+        for string in strs:
+            letters.append(str(len(string)))
+            output_list.append(string) 
+        output = "".join(output_list) 
+        letter_string = ",".join(letters)
+        output = str(num_str) +"/" + letter_string + "," + output
+        # print(output)
+        return output
+        
+
+    def decode(self, s: str) -> List[str]:
+        output = []
+        start = 0
+        num_strs_list = []
+        for letter in s:
+            start += 1
+            if letter == "/":
+                break
+            num_strs_list.append(letter)
+        num_strs = "".join(num_strs_list)
+
+        comma = int(num_strs) 
+        count = 0
+        letters_string = []
+        number = ''
+
+        for j,num in enumerate(s[start:]):
+            if count == comma:
+                print(count, comma, j)
+                break
+            if num == ",":
+                count += 1
+                letters_string.append(int(number))
+                number = ''
+                continue
+            number = number + num 
+            
+        index = j+start
+        for num in letters_string:
+            word = s[index:index + num ]
+            output.append(word)
+            index = index + num
+        return output

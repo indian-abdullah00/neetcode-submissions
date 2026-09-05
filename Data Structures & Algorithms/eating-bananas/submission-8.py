b49fbfd2494e4339c8bcd73bf64eb1741068f50f
Kashif Ayaz Khan <kashifayaz5252@gmail.com>
2026-09-05 17:13:00 +0100

    Add: eating-bananas - submission-8

---
+++ Data Structures & Algorithms/eating-bananas/submission-8.py
@@ -0,0 +1,37 @@
+from math import ceil
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        max_sum = sum(piles)
+        max_banana = max(piles)
+        min_banana= ceil(max_sum/h)
+
+        def time_eat(pile, slot):
+            piles = pile.copy()
+            count = 0
+            for item in piles:
+                count += ceil(item/slot)
+            return count
+
+        l = min_banana
+        r = max_banana+1
+        while l <= r:
+            mid = (l+r)//2
+            print(mid,l,r)
+            
+            if time_eat(piles,mid) > h:
+                l = mid+1
+            elif time_eat(piles,mid) < h:
+                r = mid-1
+                current_best = mid
+            elif time_eat(piles,mid) == h:
+                r = mid-1
+                current_best = mid
+            # print("time:",time_eat(piles,mid),"number of bananas: ", current_best)
+        return current_best
+
+                
+
+
+
+
+        
